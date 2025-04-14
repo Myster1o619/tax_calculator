1fb66e8f351477d5e872b739bc9487a26abae3da
Myster1o619 <myster1o619@hotmail.com>
2025-04-14 11:05:10 +0200

    added import and additional prices struct logic for testing

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"example.com/tax_calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
+	priceList := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.10, 0.15}
+
+	testIncludedPrices := prices.NewTaxIncludedPrice(0.10)
+
+	testIncludedPrices.Process()
+	fmt.Println("testIncludedPrices.TaxIncludedPrices:", testIncludedPrices.TaxIncludedPrices)
 
 	result := make(map[float64][]float64)
 
 	for _, tax := range taxRates {
 		adjustedPrices := []float64{}
-		for _, price := range prices {
+		for _, price := range priceList {
 			adjusted := price * (1 + tax)
 			adjustedPrices = append(adjustedPrices, adjusted)
 		}
--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,34 @@
 package prices
 
-type TaxIncluded struct {
+import (
+	"fmt"
+	"strconv"
+)
+
+type TaxIncludedPrice struct {
 	TaxRate float64
 	InputPrices []float64
-	TaxIncludedPrices[]float64
+	TaxIncludedPrices map[string][]float64
+}
+
+func (taxIncludedPrice *TaxIncludedPrice) Process() {
+	result := make(map[string][]float64)
+	stringKey := strconv.FormatFloat(taxIncludedPrice.TaxRate, 'g', 2, 64)
+	var adjustedPrices []float64 = []float64{}
+
+	for _, price := range taxIncludedPrice.InputPrices {
+		adjusted := price * (1 + taxIncludedPrice.TaxRate)
+		adjustedPrices = append(adjustedPrices, adjusted)
+	}
+
+	result[stringKey] = adjustedPrices
+	fmt.Println("RESULT:", result)
+	taxIncludedPrice.TaxIncludedPrices = result
+}
+
+func NewTaxIncludedPrice(taxRate float64) *TaxIncludedPrice {
+	return &TaxIncludedPrice{
+		TaxRate: taxRate,
+		InputPrices: []float64{10, 20, 30},
+	}
 }
