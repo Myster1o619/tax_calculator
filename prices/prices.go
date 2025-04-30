f8071ee0e8e18982d10350204b2b2908704cb534
Myster1o619 <myster1o619@hotmail.com>
2025-04-30 09:45:41 +0200

    move getPricesFromFile() logic to TaxIncludedPrice struct

--- prices/prices.go
+++ prices/prices.go
@@ -3,6 +3,9 @@ package prices
 import (
 	"fmt"
 	"strconv"
+	"math"
+	"os"
+	"bufio"
 )
 
 type TaxIncludedPrice struct {
@@ -11,13 +14,38 @@ type TaxIncludedPrice struct {
 	TaxIncludedPrices map[string][]float64
 }
 
+func (taxIncludedPrice *TaxIncludedPrice) getPricesFromFile() {
+	file, _ := os.Open("prices.txt")
+	defer file.Close()
+	pricesList := []float64{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		convertedLine, _ := strconv.ParseFloat(line, 64)
+		pricesList = append(pricesList, convertedLine)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return 
+	}
+
+	taxIncludedPrice.InputPrices = pricesList
+}
+
 func (taxIncludedPrice *TaxIncludedPrice) Process() {
+
+	taxIncludedPrice.getPricesFromFile()
+
 	result := make(map[string][]float64)
 	stringKey := strconv.FormatFloat(taxIncludedPrice.TaxRate, 'g', 2, 64)
 	adjustedPrices := []float64{}
 
 	for _, price := range taxIncludedPrice.InputPrices {
 		adjusted := price * (1 + taxIncludedPrice.TaxRate)
+		adjusted = math.Round(adjusted * 100) / 100
 		adjustedPrices = append(adjustedPrices, adjusted)
 	}
 
@@ -29,6 +57,5 @@ func (taxIncludedPrice *TaxIncludedPrice) Process() {
 func NewTaxIncludedPrice(taxRate float64) *TaxIncludedPrice {
 	return &TaxIncludedPrice{
 		TaxRate: taxRate,
-		InputPrices: []float64{10, 20, 30},
 	}
 }
